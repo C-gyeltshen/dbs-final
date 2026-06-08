6b07668f4c69de9271fed4a9d6cd6002c251ae34
C-gyeltshen <02230279.cst@rub.edu.bt>
2026-06-08 22:49:32 +0600

    wip: final

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,6 +21,7 @@ app.use(
       'http://127.0.0.1:3000',
       'http://localhost:3001',
       'http://127.0.0.1:3001',
+      'https://final-dbs.netlify.app',
     ],
     allowHeaders: ['Content-Type', 'Authorization'],
     allowMethods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
